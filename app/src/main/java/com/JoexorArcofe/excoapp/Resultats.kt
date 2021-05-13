f9711ef89e54a1cc36b926e756d153f8835f42c3
joexor <joexor@bemen3.cat>
2021-05-13 17:03:38 +0200

    Resultats interactua altres pagines

--- app/src/main/java/com/JoexorArcofe/excoapp/Resultats.kt
+++ app/src/main/java/com/JoexorArcofe/excoapp/Resultats.kt
@@ -3,15 +3,18 @@ package com.JoexorArcofe.excoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class Resultats : AppCompatActivity() {
+class Resultats : AppCompatActivity(), View.OnClickListener {
 
     private var correcte: TextView? = null
     private var incorrecte: TextView? = null
+    private var inicio: Button? = null
+    private var reintentar: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,8 @@ class Resultats : AppCompatActivity() {
 
         correcte =  findViewById<TextView>(R.id.NumAciertos)
         incorrecte =  findViewById<TextView>(R.id.NumFallos)
+        inicio = findViewById<Button>(R.id.GoInici)
+        reintentar = findViewById<Button>(R.id.Restart)
 
         var currentuser = Firebase.auth.currentUser
         var usuario = findViewById<TextView>(R.id.usuario)
@@ -32,6 +37,24 @@ class Resultats : AppCompatActivity() {
 
         correcte!!.setText(numCorrect)
         incorrecte!!.setText(numWrong)
+        inicio!!.setOnClickListener(this)
+        reintentar!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.GoInici -> {
+                val intent = Intent(this,Inici::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+            }
+
+            R.id.Restart -> {
+                val intent = Intent(this,Quiz::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+            }
+        }
     }
 
 
