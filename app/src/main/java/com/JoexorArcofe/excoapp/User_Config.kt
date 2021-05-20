63e24b94500b8ba7d37a32d0b2e76d5ed6d0db71
joexor <joexor@bemen3.cat>
2021-05-20 16:00:57 +0200

    Configuacion usuario falta verificacion contrasenya

--- app/src/main/java/com/JoexorArcofe/excoapp/User_Config.kt
+++ app/src/main/java/com/JoexorArcofe/excoapp/User_Config.kt
@@ -2,35 +2,71 @@ package com.JoexorArcofe.excoapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import java.lang.reflect.Modifier
+
 
 class User_Config : AppCompatActivity(), View.OnClickListener {
 
     private var btnAtras: ImageButton? = null
+    private var btnModificar: Button? = null
     private var textEmail: EditText? = null
+    private var textUser: EditText? = null
+    private var password: EditText? = null
+    private var repeatPwd: EditText? = null
+    private var currentuser = Firebase.auth.currentUser
+    private var username: String? = null
+    private var correo: String? = null
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_config)
 
         btnAtras = findViewById<ImageButton>(R.id.atras)
+        btnModificar = findViewById<Button>(R.id.modificar)
         textEmail = findViewById<EditText>(R.id.email)
+        textUser = findViewById<EditText>(R.id.user)
+        password = findViewById<EditText>(R.id.password)
+        repeatPwd = findViewById<EditText>(R.id.repeatPassword)
 
-        var currentuser = Firebase.auth.currentUser
+
         var usuario = findViewById<TextView>(R.id.usuario)
-        val user = currentuser?.email?.split('@')
-        usuario.text = user?.get(0)
+        /*val user = currentuser?.email?.split('@')
+        usuario.text = user?.get(0)*/
 
         btnAtras!!.setOnClickListener(this)
+        btnModificar!!.setOnClickListener(this)
+
+        db.collection("usuarios").document(currentuser.email.toString())
+                .get().addOnSuccessListener { document ->
+                    if (document != null) {
+                        username = document.getString("user")
+                        correo = document.getString("email")
+                        textEmail!!.setText(correo)
+                        textUser!!.setText(username)
+                        usuario.text = username
+                        Log.d("", "DocumentSnapshot data: ${document.data}")
+                    } else {
+                        Log.d("", "No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d("", "get failed with ", exception)
+                }
+
+        println("CORREO:" + correo)
+        println("USUARIO:" + username)
+        /*textEmail!!.setText(correo)
+        textUser!!.setText(username)*/
     }
 
     override fun onClick(v: View?) {
@@ -39,6 +75,13 @@ class User_Config : AppCompatActivity(), View.OnClickListener {
                 val intent = Intent(this, Ajustes::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                 startActivity(intent)
+            }
+            R.id.modificar -> {
+
+                db.collection("usuarios").document(currentuser.email.toString())
+                        .update("user",textUser?.text?.toString())
+                        .addOnSuccessListener { Log.d("", "DocumentSnapshot successfully updated!") }
+                        .addOnFailureListener { e -> Log.w("", "Error updating document", e) }
             }
         }
     }
